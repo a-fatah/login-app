e984b45eebb026e05d74e6b032103023c4e7433f
Abdul Fatah <abdulfatah66@gmail.com>
2021-03-15 13:21:03 +0500

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,81 @@
-import Nav from "react-bootstrap/Nav";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
-import { useReducer } from "react";
+import React, { useReducer } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { AuthContext } from "./Context";
+import NavBar from "./component/NavBar";
 import Login from "./component/Login";
+import Home from "./component/Home";
 
-function App() {
-  function reducer(state, action) {
-    switch (action.type) {
-      case "login":
-        return {
-          loggedIn: true,
-        };
-      case "logout":
-        return {
-          loggedIn: false,
-        };
-
-      default:
-        throw new Error("Invalid action type");
-    }
+function reducer(state, action) {
+  debugger;
+  switch (action.type) {
+    case "login":
+      return login(action.payload);
+    case "logout":
+      return logout();
+    default:
+      throw new Error("invalid action");
   }
+}
 
-  const initialState = {
+function login({ username, password }) {
+  localStorage.clear();
+  if (password === "s3cr3t") {
+    localStorage.setItem("loggedIn", true);
+    localStorage.setItem("username", username);
+    return {
+      loggedIn: true,
+      username,
+    };
+  }
+  return {
+    loggedIn: false,
+    error: "Invalid Credentials",
+  };
+}
+
+function logout() {
+  localStorage.clear();
+  return {
     loggedIn: false,
   };
+}
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    loggedIn: localStorage.getItem("loggedIn"),
+    username: localStorage.getItem("username"),
+  });
 
   return (
-    <Router>
-      <Nav
-        className="justify-content-end"
-        onSelect={(key) => {
-          //todo logout
-        }}
-      >
-        {state.loggedIn ? (
-          <Nav.Item>
-            <Nav.Link eventKey="logout">Logout</Nav.Link>
-          </Nav.Item>
-        ) : (
-          <Nav.Item>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-          </Nav.Item>
-        )}
-      </Nav>
-      <div className="d-flex justify-content-center m-2">
-        <Switch>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <AuthContext.Provider value={{ state, dispatch }}>
+      <Router>
+        <NavBar />
+        <div className="d-flex justify-content-center m-2">
+          <Switch>
+            <Route exact path="/">
+              {() => {
+                return state.loggedIn ? (
+                  <Redirect to="/home" />
+                ) : (
+                  <Redirect to="/login" />
+                );
+              }}
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
